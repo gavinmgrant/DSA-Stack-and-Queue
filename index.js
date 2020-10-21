3c36493f85a820b86f58c05a0af27acaa71b494c
Gavin Grant <gavingrant@gmail.com>
2020-10-21 14:57:43 -0700

    feat(palindromes): add function

--- index.js
+++ index.js
@@ -38,8 +38,48 @@ const display = stack => {
 
     return result;
 };
-
+/*
 console.log(peek(starTrek));
 const testStack = new Stack();
 console.log(isEmpty(testStack));
 console.log(display(starTrek));
+*/
+
+// 3. Check for palindromes using a stack
+
+const is_palindrome = s => {
+    s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+    // if stack is empty return null
+    if (!s) {
+        return null;
+    }
+    
+    const forward = new Stack();
+    const backward = new Stack();
+    // iterate through string and add new forward stack
+    for (let i = s.length-1; i >= 0; i--) {
+        forward.push(s[i]);
+    }
+    // iterate through string and add to new backward stack
+    for (let i = 0; i < s.length; i++) {
+        backward.push(s[i]);
+    }
+
+    // check if each letter in both stacks above are equal
+    while(!isEmpty(forward)) {
+        if(peek(forward) === peek(backward)) {
+            forward.pop();
+            backward.pop();
+        } else {
+            return false
+        }
+    }
+
+    return true;
+};
+
+// Expected answers: true, true, true, false
+console.log(is_palindrome("dad"));
+console.log(is_palindrome("A man, a plan, a canal: Panama"));
+console.log(is_palindrome("1001"));
+console.log(is_palindrome("Tauhida"));
